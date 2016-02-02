518141a7f6f2a7a419af12f34a3fa151b873ee63
leandro melo <leandro melo>
2016-02-02 21:50:15 +0900

    ex42

--- ex42.py
+++ ex42.py
@@ -2,48 +2,48 @@
 class Animal(object):
     pass
 
-## ??
+## is-a
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## has-a
         self.name = name
 
-## ??
+## is-a
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## has-a
         self.name = name
 
-## ??
+## is-a
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## has-a
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## is-a
 class Employee(Person):
 
     def __init__(self, name, salary):
-        ## ?? hmm what is this strange magic?
+        ## has-a hmm what is this strange magic?
         super(Employee, self).__init__(name)
-        ## ??
+        ## has-a
         self.salary = salary
 
-## ??
+## is-a
 class Fish(object):
     pass
 
-## ??
+## is-a
 class Salmon(Fish):
     pass
 
-## ??
+## is-a
 class Halibut(Fish):
     pass
 
@@ -51,26 +51,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Dog
 satan = Cat("Satan")
 
-## ??
+## mary is-a Dog
 mary = Person("Mary")
 
-## ??
+## has-a
 mary.pet = satan
 
-## ??
+## has-a
 frank = Employee("Frank", 120000)
 
-## ??
+## has-a
 frank.pet = rover
 
-## ??
+## is-a
 flipper = Fish()
 
-## ??
+## is-a
 crouse = Salmon()
 
-## ??
+## is-a
 harry = Halibut()
